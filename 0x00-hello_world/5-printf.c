ed572ef0ce8bd550bbbc77af48328c3bf2277e7e
EyuReaper <EyuReaper@gmail.com>
2023-02-17 13:59:39 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <std.h>
+/**
+ * main - a program that prints a line with printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art");
+return (0);
+}
+
